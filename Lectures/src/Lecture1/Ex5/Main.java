f98ad21f25c8ee98892596c806f303c52e45d1f6
PlayerThird030 <verevka.rud@gmail.com>
2023-04-15 23:22:22 +0300

    Лекция 1

---
+++ Lectures/src/Lecture1/Ex5/Main.java
@@ -0,0 +1,14 @@
+package Lecture1.Ex5;
+
+public class Main {
+    public static void main(String[] args) {
+    Magician hero1 = new Magician();
+        System.out.println(hero1);
+
+        Priest hero2 = new Priest();
+        System.out.println(hero2);
+
+        Priest hero3 = new Priest();
+        System.out.println(hero3);
+    }
+}
