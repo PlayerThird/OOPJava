c4b7dbe372b20912f481236f5c3231ee658d9dee
PlayerThird030 <verevka.rud@gmail.com>
2023-04-27 22:43:25 +0300

    Дз к 4му семинару (топорный генератор)

---
+++ HomeWork/src/Hm4/BoxCompare.java
@@ -0,0 +1,8 @@
+package Hm4;
+
+import java.util.Comparator;
+
+public interface BoxCompare extends Comparator {
+
+    int compare(Box o);
+}
---
+++ HomeWork/src/Hm4/FullBoxes.java
@@ -0,0 +1,7 @@
+package Hm4;
+
+import java.util.ArrayList;
+
+public interface FullBoxes<T extends Fruit> {
+    public ArrayList<T> GenerateFruit(int col);
+}
