540ba539307b3338b8e8bd312c5dbc3bd72d083d
PlayerThird030 <verevka.rud@gmail.com>
2023-04-30 14:10:14 +0300

    Лекция 3

---
+++ Lectures/src/Lecture3/Ex5/SalaryComporator.java
@@ -0,0 +1,11 @@
+package Lecture3.Ex5;
+
+import java.util.Comparator;
+
+public class SalaryComporator implements Comparator<Worker> {
+
+    @Override
+    public int compare(Worker o1, Worker o2) {
+        return Integer.compare(o1.salary, o2.salary);
+    }
+}
