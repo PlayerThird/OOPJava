79e09b6a037ffd8bbc7c22a2f143ca2464be94f0
PlayerThird030 <verevka.rud@gmail.com>
2023-04-30 14:10:27 +0300

    Семинар 5

---
+++ Seminars/src/Sem5/Ex1/Model/TableModel.java
@@ -0,0 +1,53 @@
+package Sem5.Ex1.Model;
+
+import Sem5.Ex1.Presenters.Model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+
+public class TableModel implements Model {
+
+    private Collection<Table> tables;//вынуждены кешировать, т.к. нет нормальной БД
+
+    public Collection<Table> loadTables() {
+        if (tables == null) {
+
+            tables = new ArrayList<>();
+
+            tables.add(new Table());
+            tables.add(new Table());
+            tables.add(new Table());
+            tables.add(new Table());
+            tables.add(new Table());
+        }
+        return tables;
+    }
+
+    public int reservationTable(Date resetionDate, int tableNo, String name) {
+        for (Table table :
+                tables) {
+            if (table.getNo() == tableNo) {
+                Reservation reservation = new Reservation(resetionDate, name);
+                table.getReservation().add(reservation);
+                return reservation.getId();
+            }
+        }
+        return -1;
+        //а можно просто ошибкой выдать
+//        throw new RuntimeException("Не корректный номер столика");
+    }
+
+    /**
+     * Действие клиента (пользователь нажал на кнопку брони),
+     * бронь столика
+     *
+     * @param oldReservation  идентификатор бронирования(старый)
+     * @param reservationDate дата бронирования
+     * @param tableNo         номер столика
+     * @param name            имя
+     */
+    public int changeReservationTeble(int oldReservation, Date reservationDate, int tableNo, String name) {
+        return -1;
+    }
+}
