686f1c9f833db7e98a3a7f2efeaf9f62e508cf49
PlayerThird030 <verevka.rud@gmail.com>
2023-05-03 12:01:18 +0300

    Заготовки 1

---
+++ Tamagochi/src/Animals/Animal.java
@@ -0,0 +1,9 @@
+package Animals;
+
+public class Animal {
+    private int eat;
+    private int whater;
+    private int health;
+    private int happy;
+    private int energy;
+}
---
+++ Tamagochi/src/Animals/Robot.java
@@ -0,0 +1,4 @@
+package Animals;
+
+public class Robot extends Animal{
+}
