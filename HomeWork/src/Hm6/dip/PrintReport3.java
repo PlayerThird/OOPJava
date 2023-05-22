fb65a174726284fde0a3fd5ed7ac976f6778ee06
PlayerThird030 <verevka.rud@gmail.com>
2023-05-22 22:20:42 +0300

    Дз к семинару 6

---
+++ HomeWork/src/Hm6/dip/PrintReport3.java
@@ -0,0 +1,11 @@
+package Hm6.dip;
+
+import java.util.List;
+
+public class PrintReport3 {
+
+    public static void saveToFile(List<ReportItem> items) {
+
+    }
+
+}
---
+++ HomeWork/src/Hm6/dip/Program.java
@@ -0,0 +1,12 @@
+package Hm6.dip;
+
+public class Program {
+
+    //TODO: (2 задание ) Доработать самостоятельно
+    public static void main(String[] args) {
+        Report report = new Report();
+        report.add(new ReportItem("Waht",5));
+        VariationPrint.print1(report);
+    }
+
+}
